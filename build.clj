9683083da0d57e4c76dadb10dc8cfb02fc66b06c
Robin Lahtinen <robin.lahtinen@gmail.com>
2025-12-15 16:07:52 +0200

    chore: update build.clj to support clojars publishing
    Signed-off-by: Robin Lahtinen <robin.lahtinen@gmail.com>

--- build.clj
+++ build.clj
@@ -4,72 +4,93 @@
             [deps-deploy.deps-deploy :as dd]))
 
 (def lib 'com.github.robinlahtinen/clj-artnet)
-(def version "0.1.0-SNAPSHOT")
-#_(def version (format "1.0.%s" (b/git-count-revs nil)))
+(def version "0.1.0")
+
+(defn- snapshot-version
+  "Append -SNAPSHOT suffix for development builds."
+  []
+  (str version "-SNAPSHOT"))
+
+(defn- get-version
+  "Return release version or snapshot version based on opts."
+  [opts]
+  (if (:release opts) version (snapshot-version)))
+
 (def class-dir "target/classes")
 
 (defn test
-      "Run all the tests."
-      [opts]
-      (let [basis (b/create-basis {:aliases [:test]})
-            cmds (b/java-command {:basis     basis,
-                                  :main      'clojure.main,
-                                  :main-args ["-m" "cognitect.test-runner"]})
-            {:keys [exit]} (b/process cmds)]
-           (when-not (zero? exit) (throw (ex-info "Tests failed" {}))))
-      opts)
+  "Run all the tests."
+  [opts]
+  (let [basis (b/create-basis {:aliases [:test]})
+        cmds (b/java-command {:basis     basis,
+                              :main      'clojure.main,
+                              :main-args ["-m" "cognitect.test-runner"]})
+        {:keys [exit]} (b/process cmds)]
+    (when-not (zero? exit) (throw (ex-info "Tests failed" {}))))
+  opts)
 
 (defn- pom-template
-       [version]
-       [[:description "A high-performance Art-Net 4 protocol implementation for Clojure."]
-        [:url "https://github.com/robinlahtinen/clj-artnet"]
-        [:licenses
-         [:license [:name "MIT License"]
-          [:url "https://github.com/robinlahtinen/clj-artnet/blob/main/LICENSE"]]]
-        [:developers [:developer [:name "Robin Lahtinen"]]]
-        [:scm [:url "https://github.com/robinlahtinen/clj-artnet"]
-         [:connection "scm:git:https://github.com/robinlahtinen/clj-artnet.git"]
-         [:developerConnection
-          "scm:git:ssh:git@github.com:robinlahtinen/clj-artnet.git"]
-         [:tag (str "v" version)]]])
+  [version]
+  [[:description "A high-performance Art-Net 4 protocol implementation for Clojure."]
+   [:url "https://github.com/robinlahtinen/clj-artnet"]
+   [:inceptionYear "2025"]
+   [:licenses
+    [:license
+     [:name "MIT"]
+     [:url "https://opensource.org/licenses/MIT"]]]
+   [:developers
+    [:developer
+     [:id "robinlahtinen"]
+     [:name "Robin Lahtinen"]
+     [:url "https://github.com/robinlahtinen"]]]
+   [:scm
+    [:url "https://github.com/robinlahtinen/clj-artnet"]
+    [:connection "scm:git:https://github.com/robinlahtinen/clj-artnet.git"]
+    [:developerConnection "scm:git:ssh:git@github.com:robinlahtinen/clj-artnet.git"]
+    [:tag (str "v" version)]]
+   [:issueManagement
+    [:system "GitHub issues"]
+    [:url "https://github.com/robinlahtinen/clj-artnet/issues"]]])
 
 (defn- jar-opts
-       [opts]
-       (assoc opts
-              :lib lib
-              :version version
-              :jar-file (format "target/%s-%s.jar" lib version)
-              :basis (b/create-basis {})
-              :class-dir class-dir
-              :target "target"
-              :src-dirs ["src"]
-              :pom-data (pom-template version)))
+  [opts]
+  (let [v (get-version opts)]
+    (println "\nVersion:" v)
+    (assoc opts
+           :lib lib
+           :version v
+           :jar-file (format "target/%s-%s.jar" lib v)
+           :basis (b/create-basis {})
+           :class-dir class-dir
+           :target "target"
+           :src-dirs ["src"]
+           :pom-data (pom-template v))))
 
 (defn ci
-      "Run the CI pipeline of tests (and build the JAR)."
-      [opts]
-      (test opts)
-      (b/delete {:path "target"})
-      (let [opts (jar-opts opts)]
-           (println "\nWriting pom.xml...")
-           (b/write-pom opts)
-           (println "\nCopying source...")
-           (b/copy-dir {:src-dirs ["resources" "src"], :target-dir class-dir})
-           (println "\nBuilding JAR..." (:jar-file opts))
-           (b/jar opts))
-      opts)
+  "Run the CI pipeline of tests (and build the JAR)."
+  [opts]
+  (test opts)
+  (b/delete {:path "target"})
+  (let [opts (jar-opts opts)]
+    (println "\nWriting pom.xml...")
+    (b/write-pom opts)
+    (println "\nCopying source...")
+    (b/copy-dir {:src-dirs ["resources" "src"], :target-dir class-dir})
+    (println "\nBuilding JAR..." (:jar-file opts))
+    (b/jar opts))
+  opts)
 
 (defn install
-      "Install the JAR locally."
-      [opts]
-      (let [opts (jar-opts opts)] (b/install opts))
-      opts)
+  "Install the JAR locally."
+  [opts]
+  (let [opts (jar-opts opts)] (b/install opts))
+  opts)
 
 (defn deploy
-      "Deploy the JAR to Clojars."
-      [opts]
-      (let [{:keys [jar-file], :as opts} (jar-opts opts)]
-           (dd/deploy {:installer :remote,
-                       :artifact  (b/resolve-path jar-file),
-                       :pom-file  (b/pom-path (select-keys opts [:lib :class-dir]))}))
-      opts)
+  "Deploy the JAR to Clojars."
+  [opts]
+  (let [{:keys [jar-file], :as opts} (jar-opts opts)]
+    (dd/deploy {:installer :remote,
+                :artifact  (b/resolve-path jar-file),
+                :pom-file  (b/pom-path (select-keys opts [:lib :class-dir]))}))
+  opts)
